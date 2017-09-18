26d94f79678defe114dc1f4e1fda67115cda2e8f
jenlij <jljohns1216@gmail.com>
2017-09-17 23:24:30 -0400

    trying to get highlight nav on scroll

--- scripts/main.js
+++ scripts/main.js
@@ -21,18 +21,47 @@ $(document).ready( () => {
 
 
   // smooth scrolling
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
+  // $('a[href*="#"]:not([href="#"])').click(function() {
+  //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+  //       || location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
+  //       var target = $(this.hash);
+  //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //         if (target.length) {
+  //           $('html,body').animate({
+  //               scrollTop: target.offset().top
+  //           }, 1000);
+  //           return false;
+  //       }
+  //   }
+  // });
+
+  var sections = $('section')
+  , nav = $('.navigation')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height - 200,
+        bottom = top + $(this).outerHeight() + 100;
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
     }
   });
 });
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
+})
